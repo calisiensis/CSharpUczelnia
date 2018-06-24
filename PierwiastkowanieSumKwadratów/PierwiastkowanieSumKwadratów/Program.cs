a3ee98a189fac6c8ad4ccd3e367192f829e1b39e
calisiensis <39063450+calisiensis@users.noreply.github.com>
2018-06-24 19:48:03 +0200

    Add files via upload
    Last mathematic project

---
+++ PierwiastkowanieSumKwadratów/PierwiastkowanieSumKwadratów/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pierwiastkowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Error.Write("a = ");
+            String s = Console.In.ReadLine();
+            double a = Convert.ToDouble(s);
+            Console.Error.Write("b = ");
+            double b = Convert.ToDouble(Console.In.ReadLine());
+            double c = Math.Sqrt(a * a + b * b); //math.sqrt - metoda statyczna liczaca pierwiastek z sum kwadratow a i b
+            Console.Out.WriteLine("{0:F3}", c);
+            //Console.Out.WriteLine("{0:F3}", Math.Sqrt(a * a + b * b)); - rozwiazanie drugie
+            Console.ReadKey();
+        }
+    }
+}
